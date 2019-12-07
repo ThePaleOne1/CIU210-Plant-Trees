c4e7eeb3764bc4649d43cc5f8eafb954eefe0929
Lachlan <mindcraftlachlan@outlook.com>
2019-12-07 22:51:34 +1100

    tree fall and turn into stump
    falling done in code instead of animator

--- Assets/TreeDecay.cs
+++ Assets/TreeDecay.cs
@@ -5,20 +5,45 @@ using UnityEngine;
 public class TreeDecay : MonoBehaviour
 {
     public GameObject tree;
+    public GameObject stump;
+
+    bool isfalling = false;
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("DestroyTree", Random.Range(3f, 60f));
+        float rand = Random.Range(3f, 60f);
+        Invoke("TreeFall", rand);
+        print(rand);
+
+        tree.SetActive(true);
+        stump.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isfalling)
+        {
+            tree.transform.RotateAround(Vector3.forward, Vector3.forward, 5);
+        }
+
+        if (tree.transform.rotation.eulerAngles.z > 90)
+        {
+            Invoke("DestroyTree", 0);
+            tree.transform.SetPositionAndRotation(transform.position, transform.rotation);
+        }
+    }
+
+    void TreeFall()
+    {
+        isfalling = true;
     }
 
     void DestroyTree()
     {
-        Destroy(tree);
+        tree.SetActive(false);
+        stump.SetActive(true);
+        isfalling = false;
     }
+
 }
