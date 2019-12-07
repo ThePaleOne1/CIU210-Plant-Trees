d1d254f3238eabe665cfa2ab0b860c7dc2a5b16e
Lachlan <mindcraftlachlan@outlook.com>
2019-12-07 22:21:17 +1100

    new scene Also new unity update

---
+++ Assets/TreeDecay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeDecay : MonoBehaviour
+{
+    public GameObject tree;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyTree", Random.Range(3f, 60f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DestroyTree()
+    {
+        Destroy(tree);
+    }
+}
