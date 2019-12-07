43eef360413ea17941500e35919e245051482331
Lachlan <mindcraftlachlan@outlook.com>
2019-12-07 21:54:38 +1100

    added testing materials

---
+++ Assets/Scripts/TreeInstantiation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeInstantiation : MonoBehaviour
+{
+    public GameObject theTree;
+    public Terrain theTerrain;
+
+    // Use this for initialization
+    void Start()
+    {
+        TerrainData theTerrainData;
+        theTerrainData = theTerrain.terrainData;
+
+       
+
+        // Grab the island's terrain data
+        
+        // For every tree on the island
+        foreach (TreeInstance tree in theTerrainData.treeInstances)
+        {
+            // Find its local position scaled by the terrain size (to find the real world position)
+            Vector3 worldTreePos = Vector3.Scale(tree.position, theTerrainData.size) + Terrain.activeTerrain.transform.position;
+            Instantiate(theTree, worldTreePos, Quaternion.identity); // Create a prefab tree on its pos
+        }
+        // Then delete all trees on the island
+        List<TreeInstance> newTrees = new List<TreeInstance>(0);
+        theTerrainData.treeInstances = newTrees.ToArray();
+    }
+}
